c509de4138b0b38420e9c1179d7648d65179cb10
Abhishekhirapara348 <abhihirapara@outlook.com>
2022-01-26 19:51:21 +0530

    Api key map

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCLTwdlGJEKJuH579DQL48nKYw3ZTwP8Q",
+  apiKey: "...",
   authDomain: "final-expo-app.firebaseapp.com",
   projectId: "final-expo-app",
   storageBucket: "final-expo-app.appspot.com",
